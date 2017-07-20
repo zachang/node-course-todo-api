96e470efdf0e6af1824facbe3026fff587f217c5
zachang <zachangdawuda@gmail.com>
2017-07-20 20:56:20 +0100

    Update script dinner

---
+++ playground/mongodb-update.js
@@ -0,0 +1,41 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+let obj = new ObjectID();
+console.log(obj);
+let url = "mongodb://localhost:27017/Todoapp";
+
+MongoClient.connect(url, (err, db) =>{
+  if(err){
+    return console.log("Unable to connect to mongoDB server", err);
+  }
+  console.log("Connected to mongoDB server");
+
+  // db.collection('Todos').findOneAndUpdate({
+  //   "_id" : ObjectID("5970b82174eb2723eef14cf3")
+  // },{
+  //     $set:{
+  //       completed:true
+  //     }
+  // },{
+  //     returnOriginal:false
+  // }).then((result) => {
+  //     console.log(result);
+  // });
+
+  db.collection('Users').findOneAndUpdate({
+    "_id" : ObjectID("596fdf5d11fd8e225c714bc9"),
+  },{
+      $set:{
+        name:"Zachang"
+      },
+      $inc:{
+        age:1
+      }
+  },{
+      returnOriginal:false
+  }).then((result) => {
+      console.log(result);
+  });
+  db.close();
+});
